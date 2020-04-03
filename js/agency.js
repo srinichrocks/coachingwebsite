4e1199f7214e7a147305562cfa6ad6847f378611
hack4kid <62446187+hack4kid@users.noreply.github.com>
2020-04-03 01:29:34 -0700

    tabs

--- js/agency.js
+++ js/agency.js
@@ -40,3 +40,15 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+
+$(document).ready(function() {
+  $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+      e.preventDefault();
+      $(this).siblings('a.active').removeClass("active");
+      $(this).addClass("active");
+      var index = $(this).index();
+      $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+      $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+  });
+});
