2730f32286ff6f4346c988747525ae1f2ecea29c
hack4kid <62446187+hack4kid@users.noreply.github.com>
2020-04-04 00:29:15 -0700

    Revert "tabs"
    This reverts commit 7baf390927a7583d69abfa21ee2ec3072c9be5f9.

--- js/agency.js
+++ js/agency.js
@@ -40,21 +40,3 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
-
-function openCity(evt, cityName){
-  var i, tabcontent, tablinks;
-
-  tabcontent=document.getElementsByClassName("tabcontent");
-  
-  for(i=0;i<tabcontent;i++){
-    tabcontent[i].style.display="none";
-  }
-
-  tablinks=document.getElementsByClassName("tablinks");
-  for(i=0;i<tablinks.length;i++){
-    tablinks[i].className=tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display="block";
-  evt.currentTarget.className+=" active";
-}
-document.getElementById("defaultOpen").click();
