7baf390927a7583d69abfa21ee2ec3072c9be5f9
hack4kid <62446187+hack4kid@users.noreply.github.com>
2020-04-04 00:26:55 -0700

    tabs

--- js/agency.js
+++ js/agency.js
@@ -40,3 +40,21 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+function openCity(evt, cityName){
+  var i, tabcontent, tablinks;
+
+  tabcontent=document.getElementsByClassName("tabcontent");
+  
+  for(i=0;i<tabcontent;i++){
+    tabcontent[i].style.display="none";
+  }
+
+  tablinks=document.getElementsByClassName("tablinks");
+  for(i=0;i<tablinks.length;i++){
+    tablinks[i].className=tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display="block";
+  evt.currentTarget.className+=" active";
+}
+document.getElementById("defaultOpen").click();
